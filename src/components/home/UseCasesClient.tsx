a1f53b72d6f07041f3495d48a8db7df02b83d35c
Vaitheeswaran Periyasamy <periyasamy.v@innovasolutions.com>
2026-04-01 12:52:52 +0530

    added iframe

--- src/components/home/UseCasesClient.tsx
+++ src/components/home/UseCasesClient.tsx
@@ -24,13 +24,16 @@ const UseCasesClient = ({ useCasesData }: UseCasesClientProps) => {
             </div>
           </div>
         </div>
+        <div className="flex items-center justify-center">
+          <iframe width="100%" height="auto" className="min-h-[300px] sm:min-h-[400px] lg:min-h-[600px] md:min-h-[500px] mb-16" frameBorder="0" src="https://www.youtube.com/embed/b-lDuufDDLs?si=PZWr8sLe2XMlkfc9" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        </div>
         <div className="grid grid-cols-12 gap-y-14 lg:gap-8">
           {useCasesData.map((useCase, index) => (
             <RevealAnimation key={useCase.slug} delay={0.4 + index * 0.1}>
               <div className="col-span-12 lg:col-span-3">
                 <div className="space-y-6">
-                  <div className="max-lg:text-center">
-                    <Image src={useCase.icon} alt="icon" width={40} height={40} className="text-secondary dark:text-accent" />
+                  <div className="flex lg:justify-start md:justify-center max-sm:justify-center">
+                    <Image src={useCase.icon} alt="icon" width={40} height={40} />
                   </div>
                   <div className="space-y-1 text-center lg:text-left">
                     <h3 className="text-heading-5 h-20">{useCase.title}</h3>

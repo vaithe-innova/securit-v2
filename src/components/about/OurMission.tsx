5dc7eed27104bb5db5c7adc5481c53cf4269d8ba
Vaitheeswaran Periyasamy <periyasamy.v@innovasolutions.com>
2026-04-09 16:49:44 +0530

    updates for about

--- src/components/about/OurMission.tsx
+++ src/components/about/OurMission.tsx
@@ -5,38 +5,38 @@ import LinkButton from '../ui/button/LinkButton';
 
 const OurMission = () => {
   return (
-    <section className="relative pt-28 pb-14 md:pt-32 md:pb-16 lg:pt-[120px] lg:pb-[70px] xl:pt-[132px] xl:pb-[80px]">
+    <section className="relative pt-32 pb-14 md:pb-16 lg:pb-[70px] xl:pt-[132px] xl:pb-[80px]">
       <div className="main-container">
         <div className="flex flex-wrap items-center gap-8 lg:flex-nowrap lg:gap-4 xl:gap-8">
-          <div className="w-full lg:w-1/2">
-          <div className="col-span-12 md:col-span-6 lg:col-span-5 gap-2">
-                <span className="text-primary-500 tracking-[2%] sm:tracking-[3%] !leading-4 text-xs sm:text-sm font-normal uppercase">About Innova</span>
-                <h2 className="text-primary-700 mt-2 mb-4 font-bold text-[30px] md:text-[34px] lg:text-[40px]">
-                  Innova Solutions is a global technology and talent partner specializing in transformative digital solutions.
-                </h2>
-                <p className="text-secondary max-w-[690px] text-sm md:text-base mb-2">
-                  Since 1998, we’ve integrated next-gen tech with our accelerated engineering methodology to help the world’s leading organizations solve complex challenges, drive rapid innovation, and achieve valuable outcomes. </p>
-                  <p className="text-secondary max-w-[690px] text-sm md:text-base">
+          <div className="w-full lg:w-1/2 ">
+            <div className="col-span-12 md:col-span-6 lg:col-span-5 gap-2">
+              <span className="text-primary-500 tracking-[2%] sm:tracking-[3%] !leading-4 text-xs sm:text-sm font-normal uppercase">About Innova</span>
+              <h2 className="text-primary-700 mt-2 mb-4 font-bold text-[30px] md:text-[34px] lg:text-[40px]">
+                Innova Solutions is a global technology and talent partner specializing in transformative digital solutions.
+              </h2>
+              <p className="text-secondary lg:max-w-[690px] text-sm md:text-base mb-2">
+                Since 1998, we’ve integrated next-gen tech with our accelerated engineering methodology to help the world’s leading organizations solve complex challenges, drive rapid innovation, and achieve valuable outcomes. </p>
+              <p className="text-secondary lg:max-w-[690px] text-sm md:text-base">
                 With curated talent communities specializing in cloud, data & analytics, digital product engineering, AI & automation, and cybersecurity, we partner with clients across industries to modernize faster and operate smarter.
-                </p>
-                <LinkButton
-                  href="https://innovasolutions.com/company/about-us/"
-                  target='_blank'
-                  insideSpan={false}
-                  className="btn rounded-full btn-primary text-white hover:btn-white-dark px-4 py-2 !leading-[24px] sm:!leading-[24px] md:!leading-[28px] lg:!leading-[32px] text-sm sm:text-base font-bold w-full sm:w-[170px] md:w-[210px] lg:w-[234px] sm:h-10 md:h-11 lg:h-12 shadow-sm mt-8">
-                  Read More
-                </LinkButton>
-              </div>
+              </p>
+              <LinkButton
+                href="https://innovasolutions.com/company/about-us/"
+                target='_blank'
+                insideSpan={false}
+                className="btn rounded-full btn-primary text-white hover:btn-white-dark px-4 py-2 !leading-[24px] sm:!leading-[24px] md:!leading-[28px] lg:!leading-[32px] text-sm sm:text-base font-bold w-full sm:w-[170px] md:w-[210px] lg:w-[234px] sm:h-10 md:h-11 lg:h-12 shadow-sm mt-8">
+                Read More
+              </LinkButton>
+            </div>
           </div>
-            <div className="w-full max-w-[650px] p-5 md:p-8">
-              <div className="flex flex-wrap gap-4 sm:flex-nowrap sm:gap-2.5">
-                <div className="">
-                  <figure>
-                  <Image src={about} alt="about"  />
+          <div className="w-full lg:w-1/2 lg: p-3">
+            <div className="flex flex-wrap gap-4 sm:flex-nowrap sm:gap-2.5">
+              <div className="w-full">
+                <figure>
+                  <Image src={about} alt="about" className='w-full h-auto' />
                 </figure>
-                </div>
               </div>
             </div>
+          </div>
         </div>
       </div>
     </section>

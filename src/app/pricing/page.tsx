ebe7c723bea80aaafdbdbc7aa43e5b679f2a8a47
kuppusamysiva <kuppusamy@sparkouttech.com>
2026-04-14 18:01:24 +0530

    Meta details updated all the pages

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -6,7 +6,9 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   ...defaultMetadata,
-  title: 'Securit - Pricing - Product Price || Innova Solutions',
+  title: 'Securit is a worker safety platform that helps monitor, detect, and respond to risks in real time | by Innova Solutions ',
+  description: 'Stay OSHA-aligned and ISO 45001-ready with real-time alerts, fall detection, incident management, and automated compliance tracking — all in one platform.',
+  keywords: 'AI worker safety platform, Worker safety monitoring, Workplace safety software, AI-powered safety management, Real-time worker monitoring, Incident management software, Risk detection platform, Workforce safety visibility, Occupational health and safety software, Safety compliance platform, AI-powered workplace safety monitoring system, AI safety assistant for supervisors.'
 };
 
 const page = () => {
